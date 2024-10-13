0060f0084273ddd7cb454126f9b884cdd5efd616
anouar garbaa <g.anouar@yahoo.com>
2024-10-13 22:05:38 +0100

    handling HTTP error

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,31 +1,41 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 // const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  // useEffect(() => {
-  //   fetch('http://localHost:3000/places').then((response) => {
-  //     return response.json()
-  //   })
-  //   .then((resData) => {
-  //     setAvailablePlaces(resData.places)
-  //   })
-  // }, [])
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localHost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      try {
+        const response = await fetch("http://localHost:3000/placesss");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        //the error handler catches all errors and uses the default message "could not fetch places, please try again later." if no specific error message is available.
+        setError({message: error.message || 'could not fetch places, please try again later.'});
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if(error) {
+    return <Error title="An error occured!" message={error.message} />
+  }
 
   return (
     <Places
@@ -38,3 +48,13 @@ export default function AvailablePlaces({ onSelectPlace }) {
     />
   );
 }
+
+
+  // useEffect(() => {
+  //   fetch('http://localHost:3000/places').then((response) => {
+  //     return response.json()
+  //   })
+  //   .then((resData) => {
+  //     setAvailablePlaces(resData.places)
+  //   })
+  // }, [])
